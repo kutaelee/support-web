907cba9d34f114aba26ef187173fc16610e817b9
kutaelee <kutaelee0@gmail.com>
2023-12-31 21:34:03 +0900

    누락된 파일 add , 이그노어 수정

---
+++ src/main/java/com/support/www/Controller/Hello.java
@@ -0,0 +1,13 @@
+package com.support.www.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Hello {
+
+    @GetMapping("/api/hello")
+    public String test() {
+        return "Hello, world!";
+    }
+}
