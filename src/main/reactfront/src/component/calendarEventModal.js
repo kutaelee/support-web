e554fab57718ea56a7ade68f18c7631c1664aebe
kutaelee <kutaelee0@gmail.com>
2024-02-01 17:56:26 +0900

    quttor modify , textarea add

--- src/main/reactfront/src/component/calendarEventModal.js
+++ src/main/reactfront/src/component/calendarEventModal.js
@@ -31,41 +31,42 @@ const CalendarEventModal = ({isOpen, onClose, onSave, initialEvent, onEventDelet
             className={`w-1/4 h-2/4 bg-gray-600 opacity-95 z-10 fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 ${isOpen
                 ? 'block'
                 : 'hidden'}`}>
-            <div className="modal-content shadow-md p-4 h-full">
-                <div className="flex justify-between items-center mb-4">
-                    <h2 className="text-lg font-semibold mb-2 text-white">이벤트 변경</h2>
+            <div className='modal-content shadow-md p-4 h-full'>
+                <div className='flex justify-between items-center mb-4'>
+                    <h2 className='text-lg font-semibold mb-2 text-white'>이벤트 변경</h2>
                     <button
-                        className="bg-red-500 text-white px-4 py-2 rounded"
+                        className='bg-red-500 text-white px-4 py-2 rounded'
                         onClick={handleDelete}>삭제</button>
                 </div>
-                <label className="block mb-2">이름
+                <label className='block mb-2'>이름
                     <input
-                        className="border rounded p-1 w-full"
-                        type="text"
+                        className='border rounded p-1 w-full'
+                        type='text'
                         value={newText}
                         onChange={(e) => setNewText(e.target.value)}/>
                 </label>
-                <label className="block mb-2">날짜
+                <label className='block mb-2'>날짜
                     <input
-                        className="border rounded p-1 w-full"
-                        type="date"
+                        className='border rounded p-1 w-full'
+                        type='date'
                         value={newDate}
                         onChange={(e) => setNewDate(e.target.value)}/>
                 </label>
-                <label className="block mb-2">설명
-                    <input
-                        className="border rounded p-1 w-full"
-                        type="text"
+                <label className='block mb-2'>설명
+                    <textarea  className='border rounded p-1 w-full h-24 max-h-40'
+                        type='textarea'
+                        maxLength='500'
                         value={newDescription}
-                        onChange={(e) => setNewDescription(e.target.value)}/>
+                        onChange={(e) => setNewDescription(e.target.value)}
+                        />              
                 </label>
                 <div className='flex justify-center'>
                 <button
-                    className="bg-blue-500 text-white px-4 py-2 rounded mr-2"
+                    className='bg-blue-500 text-white px-4 py-2 rounded mr-2'
                     onClick={handleSave}>저장</button>
 
                 <button
-                    className="bg-gray-300 text-gray-700 px-4 py-2 rounded"
+                    className='bg-gray-300 text-gray-700 px-4 py-2 rounded'
                     onClick={onClose}>닫기</button>
                 </div>
             </div>
